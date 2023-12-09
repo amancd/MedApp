7ff674c69ef4a0f4f58a8d8e721d9c90d8d82a4d
amancd <imamanwp@gmail.com>
2023-12-09 23:49:10 +0530

    Second Commit

---
+++ lib/healthrecords/userhealthrecords.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class UserRecords extends StatefulWidget {
+  const UserRecords({Key? key}) : super(key: key);
+
+  @override
+  State<UserRecords> createState() => _UserRecordsState();
+}
+
+class _UserRecordsState extends State<UserRecords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
